e77f7eeaa986a27314c66e99ed06a4572361f368
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-11-01 14:11:25 -0400

    commit tree 2ea29e41a2c8943be559b5a1bc611f809a79b8b9

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+
+focusLog() {
+    console.log('Good!')
+}
+
+blurLog() {
+    console.log('Hey! Eyes on me!')
+}
+
+    render() {
+    return (
+        <button
+        onFocus={this.focusLog}
+        onBlur={this.blurLog}>
+        </button>
+    )
+}
+}
